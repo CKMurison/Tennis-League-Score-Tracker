66122a1a5ec985bc8d6e13fd20d0ab6a4732b104
Cameron Murison <cmuris4@gmail.com>
2023-10-17 15:57:35 +0100

    changes in serializer

--- backend/api/serializer.py
+++ backend/api/serializer.py
@@ -2,5 +2,8 @@ from rest_framework import serializers
 from .models import *
 
 class ReactSerializer(serializer.ModelSerializer):
-
+    class meta:
+        model = React
+        fields = ['name', 'played', 'wins', 'draws', 'losses', 'games_for', 'games_against', 
+                  'game_difference', 'total_points', 'form', 'bbb', 'wsb', 'tsb', 'bp', 'jvm']
     
