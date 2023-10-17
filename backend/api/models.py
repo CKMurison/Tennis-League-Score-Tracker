d11d8b2b1d66b37feba4db2bcf14b1489f109cd0
Cameron Murison <cmuris4@gmail.com>
2023-10-17 12:07:11 +0100

    models created for backend, now need to link them with SQLlite and start thinking about fetch requests on the frontend

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class Player(models.Model):
+    name = models.CharField(max_length=100)
+    played = models.IntegerField()
+    wins = models.IntegerField()
+    draws = models.IntegerField()
+    losses = models.IntegerField()
+    games_for = models.IntegerField()
+    games_against = models.IntegerField()
+    game_difference = models.IntegerField()
+    total_points = models.FloatField()
+    form = models.CharField(max_length=10)
+    bbb = models.IntegerField()
+    wsb = models.IntegerField()
+    tsb = models.IntegerField()
+    bp = models.IntegerField()
+    jvm = models.FloatField()
