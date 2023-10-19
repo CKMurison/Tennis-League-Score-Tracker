9761b9d2b9beb0a443ee9cfd50d6b652d8bcca0e
Cameron Murison <cmuris4@gmail.com>
2023-10-19 10:29:34 +0100

    added get and post method to views to get and display all info from Player object

---
+++ README.md
@@ -0,0 +1,2 @@
+To run the virtual env, run source venv/bin/activate 
+use deactivate to turn it off
--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from . models import *
+from . serializer import *
+from rest_framework.response import Response
 
-# Create your views here.
+class ReactView(APIView):
+    def get(self, request):
+        players = Player.objects.all()
+        serializer = ReactSerializer(players, many=True)  
+        return Response(serializer.data)
+    
+    def post(self, request):
+        serializer = ReactSerializer (data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save ()
+            return Response(serializer .data)
